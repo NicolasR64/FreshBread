6390f5793b138d771357e8bccf1ebd37e87fe263
NicolasR64 <nicolasravaux64@gmail.com>
2022-11-14 19:00:52 +0100

    init project

---
+++ src/main/java/com/helha/FreshBread/FreshBreadApplication.java
@@ -0,0 +1,13 @@
+package com.helha.FreshBread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshBreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshBreadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helha/FreshBread/FreshBreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helha.FreshBread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshBreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
