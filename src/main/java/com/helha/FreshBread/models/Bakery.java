1b9b68847e92f4643a579ec8136cb0539b87893e
NicolasR64 <nicolasravaux64@gmail.com>
2022-11-24 15:34:49 +0100

    add lombok to bakery

--- src/main/java/com/helha/FreshBread/models/Bakery.java
+++ src/main/java/com/helha/FreshBread/models/Bakery.java
@@ -1,8 +1,21 @@
 package com.helha.FreshBread.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
 public class Bakery {
     // Attribute
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String name;
     private String address;
@@ -11,36 +24,12 @@ public class Bakery {
 
     // Contructor
 
+    public Bakery(){}
+
     public Bakery(String name, String address, int userId) {
         this.name = name;
         this.address = address;
         this.userId = userId;
-    }
-
-    // Getters and Setters
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public int getUserId() {
-        return userId;
     }
 
 }
