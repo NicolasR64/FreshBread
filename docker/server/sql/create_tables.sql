5a46d20ac4af5fa4e79d485cbf63364a26307ae9
JamieEmina <la207501@student.helha.be>
2022-11-28 10:14:37 +0100

    Ajout de keycloak

---
+++ docker/server/sql/create_tables.sql
@@ -0,0 +1,7 @@
+create user keycloak with password '1234';
+\connect helha_freshbread;
+create schema keycloak;
+GRANT CONNECT ON DATABASE helha_freshbread TO keycloak;
+GRANT USAGE ON SCHEMA keycloak TO keycloak;
+GRANT CREATE ON SCHEMA keycloak TO keycloak;
+GRANT SELECT ON ALL TABLES IN SCHEMA keycloak TO keycloak;
